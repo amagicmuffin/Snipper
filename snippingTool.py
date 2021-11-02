8ef27347c04936ddbdd8fcab2d6578f12e535969
amagicmuffin <62270558+amagicmuffin@users.noreply.github.com>
2021-11-02 10:33:14 -0700

    refactor code
    object oriented approach. don't have my raspi on me so it might be broken now tho

--- snippingTool.py
+++ snippingTool.py
@@ -1,16 +1,30 @@
+"""Snipper
+
+This linux app built for the raspberry pi functions like window's snipping tool application.
+"""
+
 import tkinter as tk
 import os
 
-def snip():
-    files = len(os.listdir('/home/pi'))
-    os.system('scrot -s')
-    if files < len(os.listdir('/home/pi')): # check if a new screenshot was added
-        snippingTool.destroy()
+SCREENSHOT_DIRECTORY = '/home/pi'
+
+class SnippingTool(master):
+    """Main app. Creates and packs a button that can take a snip."""
+    def __init__(self, master):
+        self.snipButton = tk.Button(master,text = 'Snip',padx = 50,pady = 10,command = self.snip)
+        
+        self.snipButton.pack(master)
+        
+    def snip():
+        """Take a snip. Check the length of /home/pi before and after the snip to make sure a new screenshot was added to that directory."""
+        files = len(os.listdir(SCREENSHOT_DIRECTORY))
+        os.system('scrot -s')
+        if files < len(os.listdir(SCREENSHOT_DIRECTORY)): # check if a new screenshot was added
+            snippingTool.destroy()
 
 
-snippingTool = tk.Tk()
-
-button = tk.Button(snippingTool,text = 'Snip',padx = 50,pady = 10,command = snip)
-button.pack()
-
-snippingTool.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApplication(root).pack()
+    root.mainloop()
+    
